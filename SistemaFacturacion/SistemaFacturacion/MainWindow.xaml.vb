487e2fcc7ee876701ac3c2631595fcb60c05d6ad
Hector-Luis <hluischimbo@gmail.com>
2016-09-07 02:46:56 -0500

    pequeñoz cambios

--- SistemaFacturacion/SistemaFacturacion/MainWindow.xaml.vb
+++ SistemaFacturacion/SistemaFacturacion/MainWindow.xaml.vb
@@ -1,3 +1,36 @@
-﻿Class MainWindow 
+﻿Imports System.Data
+Imports System.Data.OleDb
+Class MainWindow
+    Private dbPath = "SistemaFacturacion.mdb"
+    Public strConexion = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" & dbPath
+    Dim dsUsuarios As DataSet
 
+
+    Private Sub frmLogin_Loaded(sender As Object, e As RoutedEventArgs) Handles frmLogin.Loaded
+        Using dbConexion As New OleDbConnection(strConexion)
+
+            Dim consulta As String = "SELECT * FROM USUARIO"
+            Dim Adapter As New OleDbDataAdapter(New OleDbCommand(consulta, dbConexion))
+
+            dsUsuarios = New DataSet("USUARIO")
+            Adapter.Fill(dsUsuarios, "USUARIO")
+
+
+        End Using
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As RoutedEventArgs) Handles btnLogin.Click
+        For Each user As DataRow In dsUsuarios.Tables("USUARIO").Rows
+            If txtUser.Text = user(1) Then
+                If pwdPass.Password = user(2) Then
+                    MessageBox.Show("BIENVENIDO")
+                    Dim admin As New frmAdmin
+                    admin.Show()
+                    Me.Hide()
+                Else
+                    MessageBox.Show("CLAVE INVÁLIDA")
+                End If
+            End If
+        Next
+    End Sub
 End Class
